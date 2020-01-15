76a90ff870013c70ba4d5868629e5fa8e0cc5d71
jay.huang <jackson0585@163.com>
2020-01-15 16:41:42 +0800

    查询user表 插入user表

---
+++ src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SampleTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testInsert() {
+        User user = new User();
+        user.setName("张三");
+        user.setAge(26);
+        user.setEmail("a@126.com");
+        int rows = userMapper.insert(user);
+        System.out.println("影响记录数： " + rows);
+
+    }
+}
